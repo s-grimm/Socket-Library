fd1c2d7d6a962e3f79d7aa8508f3369a39132f02
Travis Gale <redmagefever@gmail.com>
2013-04-07 23:24:23 -0400

    Header and pragma comments for build lib type

---
+++ SocketLibrary/SocketLibrary.hpp
@@ -0,0 +1,16 @@
+#ifndef GUARD__SocketLibrary_hpp__
+#define GUARD__SocketLibrary_hpp__
+
+#if defined(_DEBUG) && !defined(_DLL)
+#pragma comment (lib,"SocketLibrary-mt-sgd.lib")
+#elif defined(_DEBUG) && defined(_DLL)
+#pragma comment (lib,"SocketLibrary-mt-gd.lib")
+#elif !defined(_DEBUG) && !defined(_DLL)
+#pragma comment (lib,"SocketLibrary-mt-s.lib")
+#elif !defined(_DEBUG) && defined(_DLL)
+#pragma comment (lib,"SocketLibrary-mt.lib")
+#endif
+
+
+
+#endif
